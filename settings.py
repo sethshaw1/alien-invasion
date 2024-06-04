8a592e8d4a5454ddd85eeebb606e196dd9437164
Seth Shaw <146257419+sethshaw1@users.noreply.github.com>
2024-06-04 12:34:05 +0100

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 700
+        self.bg_colour = (230, 230, 230)
+        # Ship settings
+        self.ship_speed = 1.5
